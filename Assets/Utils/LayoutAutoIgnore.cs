812c62123fd67c0ce1b88c29f6c5eb05a5d6d715
tnqiang <rsqmail@163.com>
2015-10-26 00:01:41 +0800

    madd layout util and test

---
+++ Assets/Utils/LayoutAutoIgnore.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace Util
+{
+	public class LayoutAutoIgnore : MonoBehaviour
+	{
+		public LayoutElement element;
+
+		void OnEnable()
+		{
+			if (null != element) {
+				element.ignoreLayout = false;
+			}
+		}
+
+		void OnDisable()
+		{
+			if (null != element) {
+				element.ignoreLayout = true;
+			}
+		}
+	}
+}
---
+++ Assets/Utils/PrefabUtil.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Log;
+
+namespace Util
+{
+	public class PrefabUtil
+	{
+		public static GameObject LoadInstantiate (string path)
+		{
+			GameObject ret = null;
+			GameObject go = Resources.Load (path) as GameObject;
+			if (null == go) {
+				Logger.Log ("gameobject at " + path + "not exist");
+			} else {
+				ret = GameObject.Instantiate (go) as GameObject;
+			}
+			return ret;
+		}
+	}
+}
