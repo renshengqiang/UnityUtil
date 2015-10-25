e5d4ae32b41d184bc58448d094508b1604abd541
tnqiang <rsqmail@163.com>
2015-10-25 22:56:16 +0800

    add Test Dir

---
+++ Assets/Tests/LifeCircle/LoadGameObject.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using Util;
+using Log;
+
+public class LoadGameObject : MonoBehaviour {
+
+	void Awake()
+	{
+		Logger.Log ("LoadGameObject Awake");
+		GameObject go = PrefabUtil.LoadInstantiate ("MonoLife");
+		if(null != go)
+		{
+			Logger.Log("Set MonoLift active = true");
+			go.SetActive(true);
+		}
+
+		MonoLife mono = go.GetComponent<MonoLife> ();
+		if (null != mono) {
+			mono.CustomFunction();
+		}
+	}
+}
---
+++ Assets/Tests/LifeCircle/MonoLife.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using Log;
+
+public class MonoLife : MonoBehaviour {
+
+	void Awake()
+	{
+		Logger.Log ("MonoLife Awake");
+	}
+	
+	void OnEnable()
+	{
+		Logger.Log ("MonoLife OnEnable");
+	}
+
+	// Use this for initialization
+	void Start () {
+		Logger.Log ("MonoLife Start");
+	}
+
+	void FixedUpdate()
+	{
+
+	}
+
+	public void CustomFunction()
+	{
+		Logger.Log ("MonoLife CustomFunction");
+	}
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void LateUpdate()
+	{
+
+	}
+
+	void OnDisable()
+	{
+
+	}
+
+	void OnDestroy()
+	{
+
+	}
+}
