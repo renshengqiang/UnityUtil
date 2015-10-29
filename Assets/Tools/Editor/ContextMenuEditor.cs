c5c1d20edb5cec8169df780bf161de62033d1e24
tnqiang <rsqmail@163.com>
2015-10-30 02:37:34 +0800

    add editor code

---
+++ Assets/Tools/Editor/ContextMenuEditor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections;
+using System.IO;
+
+public class ContextMenuEditor : Editor {
+	[MenuItem("Assets/Create/ExtendBehavior")]
+	private static void CreateExtendBehavior()
+	{
+		_CreateExtendBehavior ("hello");
+	}
+
+	private static void _CreateExtendBehavior(string behaviorName)
+	{
+		string content = System.IO.File.ReadAllText(@Application.dataPath + "/Utils/ExtendBehaviorTemplate.txt");
+		string[] guids = Selection.assetGUIDs;
+		string path = string.Empty;
+		foreach (string guid in guids) {
+			path = AssetDatabase.GUIDToAssetPath (guid);
+			break;
+		}
+		if (false == Directory.Exists (path)) 
+		{
+			path = System.IO.Path.GetDirectoryName (path);
+		}
+		Debug.Log (content);
+		string.Format (content, "behaviorName");
+		File.WriteAllText (path + "/" + behaviorName+ ".cs", content);
+		AssetDatabase.Refresh ();
+	}
+}
---
+++ Assets/Utils/ExtendBehavior.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Util
+{
+	public class ExtendBehavior : MonoBehaviour
+	{
+		private Transform _cachedTranform;
+		public Transform cachedTransform
+		{
+			get{
+				if(null == _cachedTranform)
+				{
+					_cachedTranform = transform;
+				}
+				return _cachedTranform;
+			}
+		}
+	}
+}
