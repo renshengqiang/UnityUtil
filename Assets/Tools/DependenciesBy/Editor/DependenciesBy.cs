507030dac2bc2f638e83f57e9a5b716db35eae70
tnqiang <rsqmail@163.com>
2015-11-03 02:04:59 +0800

    Complete Dependendies By

--- Assets/Tools/DependenciesBy/Editor/DependenciesBy.cs
+++ Assets/Tools/DependenciesBy/Editor/DependenciesBy.cs
@@ -1,22 +1,47 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System;
+using System.IO;
 using System.Reflection;
+using UnityEditor;
+using UnityEngine;
 
 [InitializeOnLoad]
-public class DependenciesBy : Editor
+public class DependenciesBy : AssetPostprocessor
 {    
 	private static ObjectRepo repo;
+	private static bool initialized = false;
+	 
 	static DependenciesBy()
 	{
-		repo = new ObjectRepo ();
-		repo.Initialize ();
+		Initialize ();
+	}
+
+	private static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets,
+	                                           string[] movedAssets, string[] movedFromAssetPaths)
+	{
+		Initialize ();
+		for (int i=0; importedAssets != null && i < importedAssets.Length; ++i) 
+		{
+			repo.ImportAsset(importedAssets[i]);
+		}
+		for (int i=0; deletedAssets != null && i < deletedAssets.Length; ++i) 
+		{
+			repo.DeleteAsset(deletedAssets[i]);
+		}
 	}
 		
+	private static void Initialize()
+	{
+		if (false == initialized)
+		{
+			repo = new ObjectRepo ();
+			repo.Initialize ();
+			initialized = true;
+		}
+	}
 	/// <summary>
-	/// 选中当前哪些对象依赖于当前所选中的对象
+	/// Select Objects which depend by selected Objects
 	/// </summary>
 	[MenuItem("Assets/Select Dependencies By")]
 	private static void SelectDependenciesBy()
@@ -32,17 +57,16 @@ public class DependenciesBy : Editor
 		}
 		for (int i=0; i<dependenciesBy.Count; ++i) 
 		{
-			Debug.Log(dependenciesBy[i]);
 			dependenciesByPaths.Add(AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(dependenciesBy[i]), typeof(UnityEngine.Object)));
 		}
 		Selection.objects = dependenciesByPaths.ToArray ();
         ShowSelectionInProjectHierarchy();
 	}
 
-    static void ShowSelectionInProjectHierarchy()
+    private static void ShowSelectionInProjectHierarchy()
     {
-        var pbType = GetType("UnityEditor.ProjectBrowser");
-        var meth = pbType.GetMethod("ShowSelectedObjectsInLastInteractedProjectBrowser",
+		Type pbType = GetType("UnityEditor.ProjectBrowser");
+		MethodInfo meth = pbType.GetMethod("ShowSelectedObjectsInLastInteractedProjectBrowser",
             BindingFlags.Public |
             BindingFlags.NonPublic |
             BindingFlags.Static);
@@ -50,34 +74,25 @@ public class DependenciesBy : Editor
     }
 
     // helper method to find a tyep of a given name
-    static Type GetType(string name)
+    private static Type GetType(string name)
     {
-        var assemblies = AppDomain.CurrentDomain.GetAssemblies();
-        foreach (var asm in assemblies)
-        {
-            var type = asm.GetType(name);
-            if (type != null)
-            {
-                return type;
-            }
-        }
-
+        Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+		for (int i=0; assemblies != null && i < assemblies.Length; ++i)
+		{
+			Type type = assemblies[i].GetType(name);
+			if (type != null)
+			{
+				return type;
+			}
+		}
         return null;
     }
 }
 
-/// <summary>
-/// Description of every ObjectItem
-/// </summary>
-public class ObjectItem
-{
-	public string selfId;
-	public List<string> depencenciesBy = new List<string>();
-}
-
 public class ObjectRepo
 {
-	private Dictionary<string, ObjectItem> dicObjecs;
+	private Dictionary<string, List<string>> dicDepBy;
+	private Dictionary<string, List<string>> dicDep;
 	private static string configFilePath = Application.dataPath + "/../Library/DependenciesBy.config";
 
 	public void Initialize()
@@ -100,48 +115,126 @@ public class ObjectRepo
 
 	public List<string> GetDepencenciesBy(string guid)
 	{
-		if (dicObjecs.ContainsKey (guid)) {
-			return dicObjecs [guid].depencenciesBy;
+		if (dicDepBy.ContainsKey (guid)) {
+			return dicDepBy [guid];
 		} else {
 			return null;
 		}
 	}
 
-	private void ParseFromRepo()
+	public void ImportAsset(string assetPath)
 	{
-		dicObjecs = new Dictionary<string, ObjectItem>();
-		string[] assets = AssetDatabase.GetAllAssetPaths ();
-		for (int i = 0; assets != null && i < assets.Length; ++i) {
-			string[] dependencies = AssetDatabase.GetDependencies(new string[]{assets[i]});
-			string guidDepBy = AssetDatabase.AssetPathToGUID(assets[i]);
-			for(int j=0; j<dependencies.Length;++j)
+		string guid = AssetDatabase.AssetPathToGUID (assetPath);
+		string[] dependencies = AssetDatabase.GetDependencies(new string[]{assetPath});
+		List<string> lstDep = null;
+
+		if (false == dicDep.TryGetValue(assetPath, out lstDep)) 
+		{
+			lstDep = new List<string>();
+			dicDep[assetPath] = lstDep;
+		}
+		for (int i=0; dependencies != null && i < dependencies.Length; ++i) 
+		{
+			if(dependencies[i] == assetPath) continue;	// Ignore myself
+
+			// add dicDepBy
+			string depGuid = AssetDatabase.AssetPathToGUID(dependencies[i]);
+			List<string> lstDepBy = null;
+			dicDepBy.TryGetValue(depGuid, out lstDepBy);
+			if(null == lstDepBy)
 			{
-				if(dependencies[j] != assets[i])
+				lstDepBy = new List<string>();
+				dicDepBy[depGuid] = lstDepBy;
+			}
+			if(false == lstDepBy.Contains(guid))
+			{
+				lstDepBy.Add(guid);
+				Debug.Log(assetPath + "Dep: " + dependencies[i]);
+			}
+
+			// add dicDep
+			if(false == lstDep.Contains(depGuid))
+			{
+				lstDep.Add(depGuid);
+			}
+		}
+
+		Serialize ();
+		Debug.Log ("Added: " + assetPath);
+	}
+
+	public void ParseFromRepo()
+	{
+		dicDepBy = new Dictionary<string, List<string>>();
+		dicDep = new Dictionary<string, List<string>> ();
+		string[] assets = AssetDatabase.GetAllAssetPaths ();
+		for (int i = 0; assets != null && i < assets.Length; ++i) 
+		{
+			ImportAsset(assets[i]);
+		}
+	}
+
+	public void DeleteAsset(string assetPath)
+	{
+		string guid = AssetDatabase.AssetPathToGUID (assetPath);
+		List<string> lstDep = null;
+		List<string> lstDepBy = null;
+
+		dicDep.TryGetValue (guid, out lstDep);
+		dicDepBy.TryGetValue (guid, out lstDepBy);
+
+		for (int i=0; lstDep != null && i<lstDep.Count; ++i) 
+		{
+			List<string> innerLstDepBy = null;
+			dicDepBy.TryGetValue(lstDep[i], out innerLstDepBy);
+			if(null == innerLstDepBy)
+			{
+				Debug.LogWarning("Assets Depencencies is broken, suggest repairt it " + AssetDatabase.GUIDToAssetPath(lstDep[i]));
+			}
+			else
+			{
+				if(innerLstDepBy.Contains(guid))
 				{
-					string guid = AssetDatabase.AssetPathToGUID(dependencies[j]);
-					ObjectItem item = null;
-					dicObjecs.TryGetValue(guid, out item);
-					if(null == item)
-					{
-						item = new ObjectItem();
-						item.selfId = guid;
-						item.depencenciesBy = new List<string>();
-						dicObjecs.Add(guid, item);
-					}
-					if(false == item.depencenciesBy.Contains(guidDepBy))
-					{
-						item.depencenciesBy.Add(guidDepBy);
-						Debug.Log(assets[i] + "Dep: " + dependencies[j]);
-					}
+					innerLstDepBy.Remove(guid);
+				}
+				else
+				{
+					Debug.LogWarning("Assets Depencencies is broken, suggest repairt it " + AssetDatabase.GUIDToAssetPath(guid));
+				}
+			}
+
+			Serialize ();
+		}
+
+		for (int i=0; lstDepBy != null && i<lstDepBy.Count; ++i) 
+		{
+			List<string> innerLstDep = null;
+			dicDep.TryGetValue(lstDepBy[i], out innerLstDep);
+			if(null == innerLstDep)
+			{
+				Debug.LogWarning("Assets Depencencies is broken, suggest repairt it " + AssetDatabase.GUIDToAssetPath(lstDepBy[i]));
+			}
+			else
+			{
+				if(innerLstDep.Contains(guid))
+				{
+					innerLstDep.Remove(guid);
+				}
+				else
+				{
+					Debug.LogWarning("Assets Depencencies is broken, suggest repairt it " + AssetDatabase.GUIDToAssetPath(guid));
 				}
 			}
 		}
+
+		Debug.Log("Deleted：" + assetPath);
 	}
 
 	private void Deserialize()
 	{
-		dicObjecs = new Dictionary<string, ObjectItem>();
-		string[] allLines = FileUtil.ReadAllLines (configFilePath);
+		dicDepBy = new Dictionary<string, List<string>>();
+		dicDep = new Dictionary<string, List<string>> ();
+		string[] allLines = File.ReadAllLines (configFilePath);
 		for (int index = 0; allLines != null && index < allLines.Length; ++index) {
 			string[] objectAndDep = allLines[index].Split(new char[]{':'}, System.StringSplitOptions.RemoveEmptyEntries);
 			if(objectAndDep.Length < 2)
@@ -150,15 +243,25 @@ public class ObjectRepo
 			}
 			else
 			{
-				ObjectItem item = new ObjectItem();
-				item.selfId = objectAndDep[0];
-				item.depencenciesBy = new List<string>();
+				List<string> lstDepBy = new List<string>();
 				string[] dependencies = objectAndDep[1].Split(new char[]{';'}, System.StringSplitOptions.RemoveEmptyEntries);
 				if(dependencies != null)
 				{
-					item.depencenciesBy.AddRange(dependencies);
+					lstDepBy.AddRange(dependencies);
 				}
-				dicObjecs.Add(item.selfId, item);
+				dicDepBy.Add(objectAndDep[0], lstDepBy);
+
+				for(int i=0; dependencies != null && i<dependencies.Length; ++i)
+				{
+					List<string> lstDep = null;
+					dicDep.TryGetValue(dependencies[i], out lstDep);
+					if(null == lstDep) 
+					{
+						lstDep = new List<string>();
+						dicDep.Add(dependencies[i], lstDep);
+					}
+					lstDep.Add(objectAndDep[0]);
+				}
 			}
 		}
 	}
@@ -167,22 +270,29 @@ public class ObjectRepo
 	{
 		string content = "";
 
-		if (null == dicObjecs) 
+		if (null == dicDepBy) 
 		{
 			Debug.LogError("Need to Initialze First");
 			return;
 		}
-		foreach (KeyValuePair<string, ObjectItem> keyValue in dicObjecs)
+		foreach (KeyValuePair<string, List<string>> keyValue in dicDepBy)
 		{
-			ObjectItem item = keyValue.Value;
-			content += item.selfId + ":";
-			for(int index = 0; item.depencenciesBy != null && index < item.depencenciesBy.Count; ++index)
+			List<string> lstDepBy = keyValue.Value;
+			content += keyValue.Key + ":";
+			for(int index = 0; lstDepBy != null && index < lstDepBy.Count; ++index)
 			{
-				content += item.depencenciesBy[index] + ";";
+				content += lstDepBy[index] + ";";
 			}
 			content += "\r\n";
 		}
 
-		FileUtil.SaveToFile (content, configFilePath);
+		using (FileStream fileStream = new FileStream(configFilePath, 
+		                                   File.Exists(configFilePath) ? FileMode.Truncate : FileMode.OpenOrCreate))
+		{
+			byte[] bytes = System.Text.Encoding.Default.GetBytes(content);
+			fileStream.Write(bytes, 0, bytes.Length);
+			fileStream.Close();
+			fileStream.Dispose();
+		}
 	}
 }
