05c23757bc47829691172f724dc1fab4c6561c95
rensq <rsqmail@163.com>
2016-01-12 19:30:21 +0800

    add physic example

---
+++ Assets/Tests/Collision/CollisionTest.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using Log;
+
+public class CollisionTest : MonoBehaviour {
+    public int id = 1;
+    void OnTriggerEnter( Collider other )
+    {
+        Logger.Log("OnTriggerEnter: " + other + " " + id);
+    } 
+
+    void OnCollisionEnter( Collision collisionInfo )
+    {
+        Logger.Log("OnCollisionEnter: " + collisionInfo + " " + id);
+    }
+}
